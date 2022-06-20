0d03553b4b74c8929a79f431a1819da555bc5c84
thales-pinner <thales.neves@pinpeople.com.br>
2022-06-19 22:32:03 -0300

    Change to private methods and improve results

--- lib/fizz_buzz.ex
+++ lib/fizz_buzz.ex
@@ -5,23 +5,25 @@ defmodule FizzBuzz do
     |> handle_file_read()
   end
 
-  def handle_file_read({:ok, result}) do
-    result
-    |> String.split(",")
-    |> Enum.map(&convert_and_evaluate_numbers/1)
-    # Enum.map(list, fn num -> String.to_integer(num) end)
-    # Enum.map(list, &String.to_integer/1) -> /1 means one parameter -> Arity = 1
+  defp handle_file_read({:ok, result}) do
+    result =
+      result
+      |> String.split(",")
+      |> Enum.map(&convert_and_evaluate_numbers/1)
+
+    {:ok, result}
   end
   
-  def handle_file_read(:error, reason), do: "Error reading file #{reason}"
+  defp handle_file_read({:error, reason}), do: {:error, "Error reading file #{reason}"}
 
-  def convert_and_evaluate_numbers(elem) do
-    number = String.to_integer(elem)
-    evaluate_numbers(number)
+  defp convert_and_evaluate_numbers(elem) do
+    elem
+    |> String.to_integer()
+    |> evaluate_numbers()
   end
 
-  def evaluate_numbers(number) when rem(number, 3) == 0 and rem(number, 5) == 0, do: :fizzbuzz
-  def evaluate_numbers(number) when rem(number, 3) == 0, do: :fizz
-  def evaluate_numbers(number) when rem(number, 5) == 0, do: :buzz
-  def evaluate_numbers(number), do: number
+  defp evaluate_numbers(number) when rem(number, 3) == 0 and rem(number, 5) == 0, do: :fizzbuzz
+  defp evaluate_numbers(number) when rem(number, 3) == 0, do: :fizz
+  defp evaluate_numbers(number) when rem(number, 5) == 0, do: :buzz
+  defp evaluate_numbers(number), do: number
 end
