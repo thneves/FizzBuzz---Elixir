604a42618897ed54712132bab15fd9f16b0068b6
thales-pinner <thales.neves@pinpeople.com.br>
2022-06-19 21:45:09 -0300

    Two ways of reading a file with pattern matching

--- lib/fizz_buzz.ex
+++ lib/fizz_buzz.ex
@@ -1,18 +1,17 @@
 defmodule FizzBuzz do
-  @moduledoc """
-  Documentation for `FizzBuzz`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> FizzBuzz.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def build(file_name) do
+    case File.read(file_name) do
+      {:ok, result} -> result
+      {:error, reason} -> reason
+    end
   end
+
+  def build_pipe(file_name) do
+    file_name
+    |> File.read()
+    |> handle_file_read()
+  end
+
+  def handle_file_read({:ok, result}), do: result
+  def handle_file_read(:error, reason), do: reason
 end
