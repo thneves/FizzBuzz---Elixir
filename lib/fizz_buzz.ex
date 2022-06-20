9c786051585d7d555ca0d14b36db957639dbb054
thales-pinner <thales.neves@pinpeople.com.br>
2022-06-19 21:55:00 -0300

    Refactor handle file methods

--- lib/fizz_buzz.ex
+++ lib/fizz_buzz.ex
@@ -5,6 +5,13 @@ defmodule FizzBuzz do
     |> handle_file_read()
   end
 
-  def handle_file_read({:ok, result}), do: result
-  def handle_file_read(:error, reason), do: reason
+  def handle_file_read({:ok, result}) do
+    result
+    |> String.split(",")
+    |> Enum.map(&String.to_integer/1)
+    # Enum.map(list, fn num -> String.to_integer(num) end)
+    # Enum.map(list, &String.to_integer/1) -> /1 means one parameter -> Arity = 1
+  end
+  
+  def handle_file_read(:error, reason), do: "Error reading file #{reason}"
 end
